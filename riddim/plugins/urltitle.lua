41dd829044de14f944d048835c1937026cc873dd
Pitriss <devnull@wjake.com>
2014-09-21 17:39:42 +0200

    Fixed bug in urltitle where non UTF8 pages was crashing bot. lua-iconv needed.

--- riddim/plugins/urltitle.lua
+++ riddim/plugins/urltitle.lua
@@ -1,6 +1,12 @@
+-- This plugin needs lua.iconv: http://luaforge.net/projects/lua-iconv/
+
 function riddim.plugins.urltitle(bot)
 	require "net.httpclient_listener";
 	local http = require "net.http";
+	local iconv = require "iconv";
+
+	local nstr = nil
+	local err = nil
 
 	local function handler(message)
 		local url = message.body and message.body:match("https?://%S+");
@@ -8,13 +14,23 @@ function riddim.plugins.urltitle(bot)
 			http.request(url, nil, function (data, code)
 				if code ~= 200 then return end
 				local title = data:match("<title[^>]*>([^<]+)");
+				local encod = string.upper(data:match('charset=(%w+[-_]%w+)'))
 
 				if title then
 					title = title:gsub("\n", "");
-					if message.room then
-						message.room:send_message(title)
-					else
-						message:reply(title);
+					if encod ~= "UTF-8" then
+						local constr = iconv.new("UTF-8", encod.."//TRANSLIT")
+						nstr, err = constr:iconv(title)
+					end
+					if err == nil and nstr ~= nil then
+						title = nstr
+					end
+					if nstr ~= nil or encod == "UTF-8" then
+						if message.room then
+							message.room:send_message(title)
+						else
+							message:reply(title);
+						end
 					end
 				end
 			end);
