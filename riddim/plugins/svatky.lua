bc2520defcd7d2cc7694044098abbf0f6188a7c5
Pitriss <devnull@wjake.com>
2017-07-31 19:22:25 +0200

    Redo svatky.lua to get data from another source as original source is gone.

--- riddim/plugins/svatky.lua
+++ riddim/plugins/svatky.lua
@@ -10,20 +10,68 @@ function riddim.plugins.svatky(bot)
 		end
 	end
 
+	-- Lets define holidays granted by law in Czech rep
+	-- http://www.mpsv.cz/cs/74
+	svatky = {}
+
+	svatky["0101"] = "Nový rok / Den obnovy samostatného českého státu"
+	svatky["0105"] = "Svátek práce"
+	svatky["0805"] = "Den vítězství"
+	svatky["0507"] = "Den slovanských věrozvěstů Cyrila a Metoděje"
+	svatky["0607"] = "Den upálení mistra Jana Husa"
+	svatky["2809"] = "Den české státnosti"
+	svatky["2810"] = "Den vzniku samostatného československého státu"
+	svatky["1711"] = "Den boje za svobodu a demokracii"
+	svatky["2412"] = "Štědrý den"
+	svatky["2512"] = "1. svátek vánoční"
+	svatky["2612"] = "2. svátek vánoční"
+
+
 	bot:hook("commands/svatek", function (command)
 		local addr = trim(command.param) or "";
 
 		if addr then
-			url = "http://www.martinsimon.cz/svatky/"
+			datum = os.date("%d%m")
+			hrdatum = os.date("%d.%m.")
+			url = "http://svatky.adresa.info/txt?date=" .. datum
 			http.request(url, nil, function (data, code)
 				if code ~= 200 then
-					command:reply("Error while contacting http://www.martinsimon.cz/svatky/ !")
+					command:reply("Error while contacting http://svatky.adresa.info/ !")
 					return
 				end
+				if data == "" then
+					svatek = "-"
+				else
+					lines = {}
+					for ss in data:gmatch("[^\r\n]+") do
+						s = ss:gsub("^[0-9]+;(.*)", "%1")
+						table.insert(lines, s)
+					end
+					fcount = 0
+					svatek = ""
+					for k,v in pairs(lines) do
+						if fcount ~= 0 then svatek = svatek .. " a " end
+						svatek = svatek .. v
+						fcount = fcount+1
+					end
+				end
+				ssvatek = nil
+				for k,v in pairs(svatky) do
+					if datum == k then
+						ssvatek = v
+					end
+				end
+				-- lets generae valid reply
+				reply = "Dnes je " .. hrdatum .. "\n"
+				if svatek ~= "-" and ssvatek ~= nil then
+					reply = reply .. "Dnes má svátek " .. svatek .. " a tento den je zároveň státním svátkem: " ..ssvatek
+				elseif svatek ~= "-" and ssvatek == nil then
+					reply = reply .. "Dnes má svátek " .. svatek
+				else
+					reply = reply .. "Tento den je státním svátkem: " .. ssvatek
+				end
 
-				svatek = string.match(data, '.*<item><title>(.*)</title>.*')
-
-				command:reply(trim(svatek))
+				command:reply(trim(reply))
 			end);
 		end
 	end);
